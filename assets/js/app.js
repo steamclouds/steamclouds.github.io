feb7ac5fc6c3a8e488bbb12ba87b413105bba3cd
steamclouds <itsmiiiandreas@gmail.com>
2025-09-17 17:05:06 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -203,3 +203,27 @@
         return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
     }
 })
+
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+
+function showSlides() {
+   
+    for (let i = 0; i < totalSlides; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+    if (slideIndex > totalSlides) { slideIndex = 1 } 
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 3000);
+}
+
+
+if (totalSlides > 0) {
+    showSlides();
+}
